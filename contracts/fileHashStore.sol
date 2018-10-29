66de5025fb2d499277d7b16c6fb1fe6686508966
郭泰彪 <jarvis@guoMBP.local>
2018-10-29 20:40:52 +0800

    create a smartcontract

---
+++ contracts/fileHashStore.sol
@@ -0,0 +1,20 @@
+//本示例使用remix和MetaMask进行开发
+
+contract fileHashStore {
+ string ipfsHash;
+
+    //将文件哈希存储到链上，需要利用MetaMask进行签名
+
+    //下面是一个叫做natspec的特殊注释，
+    //由3个连续的斜杠标记，当询问用户确认交易事务时将显示。
+
+    ///本交易将使一个将文件哈希存储到链上的智能合约上链
+ function sendHash(string fileHash) public {
+   ipfsHash = fileHash;
+ }
+
+    //在区块链上查询文件
+ function getHash() public view returns (string fileHash) {
+   return ipfsHash;
+ }
+}
